55e843f88cd8461adaa05f472e799c19e05ddeb6
Mikael Bergman <mikael@papaja.se>
2023-12-02 15:54:46 +0100

    Day 2 done
    And some clean up on day 1

--- Sources/Day01.swift
+++ Sources/Day01.swift
@@ -14,58 +14,50 @@ struct Day01: AdventDay {
 
     // Splits input data into its component parts and convert from string.
     var entities: [String] {
-        data.split(separator: "\n").compactMap {
-            "\($0)"
-        }
+        data.split(separator: "\n").compactMap { "\($0)" }
     }
 
     func part1() -> Any {
-        return entities.map { entity in
-            getIntValue(entity)
-        }.reduce(0, +)
+        return entities.map { getIntValue($0) }.reduce(0, +)
     }
 
     func part2() -> Any {
-        return entities.map { entity in
-            getIntValue(replaceValuesInString(entity))
-        }.reduce(0, +)
+        return entities.map { getIntValue(replaceValuesInString($0)) }.reduce(0, +)
     }
 
+    // Get first and last number in string and return as Int
     private func getIntValue(_ input: String) -> Int {
+        // Remove all non digits and split into array
         let values = input.components(separatedBy: CharacterSet.decimalDigits.inverted).joined().map { "\($0)" }
         guard let first = values.first, let last = values.last else { return 0 }
         return Int("\(first)\(last)") ?? 0
     }
 
+    // Find all number words in string and insert number value at starting index of found word
     private func replaceValuesInString(_ input: String) -> String {
         var returnString = input
         var foundValues = [(index: Int, value: Int)]()
-        let rangevalues: [(String, Int)] = [
-            ("one", 1),
-            ("two", 2),
-            ("three", 3),
-            ("four", 4),
-            ("five", 5),
-            ("six", 6),
-            ("seven", 7),
-            ("eight", 8),
-            ("nine", 9)
-        ]
-        for value in rangevalues {
+        let rangevalues: [String] = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine",]
+        for (index, value) in rangevalues.enumerated() {
             var searchStartIndex = input.startIndex
+
+            // While loop is needed so multiple instances of a word can be found
             while searchStartIndex < input.endIndex,
-                  let range = input.range(of: value.0, range: searchStartIndex ..< input.endIndex),
+                  let range = input.range(of: value, range: searchStartIndex ..< input.endIndex),
                   !range.isEmpty
             {
-                let index = input.distance(from: input.startIndex, to: range.lowerBound)
-                foundValues.append((index, value.1))
+                let startIndex = input.distance(from: input.startIndex, to: range.lowerBound)
+                foundValues.append((startIndex, index + 1))
                 searchStartIndex = range.upperBound
             }
         }
+
+        // Sort found values by index and insert number value at index
         let sortedValues = foundValues.sorted { $0.index < $1.index }
         for (index, value) in sortedValues.enumerated() {
             returnString.insert(contentsOf: "\(value.value)", at: returnString.index(returnString.startIndex, offsetBy: value.index + index))
         }
+
         return returnString
     }
 }
---
+++ Sources/Day02.swift
@@ -0,0 +1,120 @@
+//
+//  Day01.swift
+//
+//
+//  Created by Mikael Bergman on 2023-12-01.
+//
+
+import Algorithms
+import Foundation
+
+struct Day02: AdventDay {
+    // Save your data in a corresponding text file in the `Data` directory.
+    var data: String
+
+    // Splits input data into its component parts and convert from string.
+    var entities: [String] {
+        data.split(separator: "\n").compactMap { "\($0)" }
+    }
+
+    func part1() -> Any {
+        var validGameIds: [Int] = []
+        for entity in entities {
+            let nonWhitespace = entity.replacingOccurrences(of: " ", with: "")
+            let game = getGameStringAndGameId(nonWhitespace)
+            guard let gameId = game.id else {
+                continue
+            }
+
+            if isGameValid(nonWhitespace.replacingOccurrences(of: game.string, with: "")) {
+                validGameIds.append(gameId)
+            }
+        }
+
+        return validGameIds.reduce(0, +)
+    }
+
+    func part2() -> Any {
+        var powers: [Int] = []
+        for entity in entities {
+            let nonWhitespace = entity.replacingOccurrences(of: " ", with: "")
+            let game = getGameStringAndGameId(nonWhitespace)
+            if game.id == nil {
+                continue
+            }
+
+            powers.append(getPowerOfGame(nonWhitespace.replacingOccurrences(of: game.string, with: "")))
+        }
+
+        return powers.reduce(0, +)
+    }
+
+    // Parse the beginning of the input to get the game string and the game id.
+    private func getGameStringAndGameId(_ input: String) -> (string: String, id: Int?) {
+        var gameString = ""
+
+        for i in 0 ..< input.count {
+            let prefix = input.prefix(i)
+            if prefix.last == ":" {
+                gameString = String(prefix)
+                break
+            }
+        }
+
+        let gameId = gameString.components(separatedBy: CharacterSet.decimalDigits.inverted).joined()
+        return (gameString, Int(gameId))
+    }
+
+    // Loop through each cubeto make sure their color values are lower than the max allowed values
+    private func isGameValid(_ input: String) -> Bool {
+        let cubeSets = getCubeSets(input)
+        let maxValues: [String: Int] = [
+            "red": 12,
+            "green": 13,
+            "blue": 14
+        ]
+
+        for cubeSet in cubeSets {
+            for cube in cubeSet {
+                let color = cube.components(separatedBy: .letters.inverted).joined()
+                let value = cube.components(separatedBy: .decimalDigits.inverted).joined()
+                if let maxValue = maxValues[color], let intValue = Int(value), intValue > maxValue {
+                    return false
+                }
+            }
+        }
+
+        return true
+    }
+
+    // Loop through each cube to get the lowest possible color amount for all sets, then return the power of all colors
+    private func getPowerOfGame(_ input: String) -> Int {
+        let cubeSets = getCubeSets(input)
+        var lowestPossibleValues: [String: Int] = [
+            "red": 0,
+            "green": 0,
+            "blue": 0
+        ]
+
+        for cubeSet in cubeSets {
+            for cube in cubeSet {
+                let color = cube.components(separatedBy: .letters.inverted).joined()
+                let value = cube.components(separatedBy: .decimalDigits.inverted).joined()
+                if let currentLowest = lowestPossibleValues[color], let intValue = Int(value), intValue > currentLowest {
+                    lowestPossibleValues[color] = intValue
+                }
+            }
+        }
+
+        return lowestPossibleValues.values.reduce(1, *)
+    }
+
+    private func getCubeSets(_ input: String) -> [[String]] {
+        return input
+            .split(separator: ";")
+            .map { cubeSet in
+                "\(cubeSet)".split(separator: ",")
+                    .map { "\($0)" }
+            }
+    }
+}
